5cad923accf2adc59036f9aba29dc02261217334
Allen Garvey <allen@allengarvey.com>
2015-12-28 20:36:01 -0500

    refactor to use pipe operator instead of intermediate variables

--- test.ex
+++ test.ex
@@ -1,11 +1,12 @@
 file_extensions = ["ex", "md", "rb"]
 #use System.user_home instead of ~ since it won't be expanded
-files = Stream.map(file_extensions, fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end)
 
-file_strings = Stream.map files, fn x -> {g,_} = x
+
+file_extensions |> 
+Stream.map(fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end) |>
+Stream.map (fn x -> {g,_} = x
 g
-end
+end) |>
+Stream.filter(fn x -> x != "" end) |>
+Enum.map(fn x -> String.split x, "\n"  end)
 
-file_strings2 = Stream.filter file_strings, fn x -> x != "" end
-
-file_string_list = Enum.map file_strings2, fn x -> String.split x, "\n"  end
