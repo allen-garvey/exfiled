19c161089d0ad9d5f4f55c00a914df9535909517
Allen Garvey <allen@allengarvey.com>
2015-12-28 20:15:58 -0500

    unpack results of find

--- test.ex
+++ test.ex
@@ -1,3 +1,9 @@
-file_extensions = ["rb", "md"]
+file_extensions = ["ex", "md", "rb"]
 #use System.user_home instead of ~ since it won't be expanded
-files = Enum.map(file_extensions, fn x -> System.cmd "find", [".", "-type", "f", "-name", "*.#{x}"]  end)
+files = Enum.map(file_extensions, fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end)
+
+file_strings = Enum.map files, fn x -> {g,_} = x
+g
+end
+
+file_string_list = Enum.map file_strings, fn x -> String.split x, "\n"  end
