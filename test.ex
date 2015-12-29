29d957a4bdbfd42121e6545bcfcf06507247edd4
Allen Garvey <allen@allengarvey.com>
2015-12-28 20:18:58 -0500

    switch to streams to increase performance

--- test.ex
+++ test.ex
@@ -1,8 +1,8 @@
 file_extensions = ["ex", "md", "rb"]
 #use System.user_home instead of ~ since it won't be expanded
-files = Enum.map(file_extensions, fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end)
+files = Stream.map(file_extensions, fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end)
 
-file_strings = Enum.map files, fn x -> {g,_} = x
+file_strings = Stream.map files, fn x -> {g,_} = x
 g
 end
 
