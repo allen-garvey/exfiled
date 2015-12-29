a2d3d22cba9ebd887ce4d881a0954aba3ad5d079
Allen Garvey <allen@allengarvey.com>
2015-12-28 20:40:08 -0500

    refactor to use built in method to get element of tuple

--- test.ex
+++ test.ex
@@ -4,9 +4,7 @@ file_extensions = ["ex", "md", "rb"]
 
 file_extensions |> 
 Stream.map(fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end) |>
-Stream.map (fn x -> {g,_} = x
-g
-end) |>
+Stream.map (fn x -> Kernel.elem x, 0 end) |>
 Stream.filter(fn x -> x != "" end) |>
 Enum.map(fn x -> String.split x, "\n"  end)
 
