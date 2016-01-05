a554b952172d0017b43403d279250a929c4ff77a
Allen Garvey <allengarvey@hotmail.com>
2016-01-04 22:52:44 -0500

    add regex for automatic categorization

--- test.ex
+++ test.ex
@@ -1,12 +1,23 @@
 file_extensions = ["ex", "md", "rb"]
 #use System.user_home instead of ~ since it won't be expanded
+categories = [{~r/\.ex/i, "Elixir"}]
 
+cat_finder = fn file, {pattern, name} -> 
+				if Regex.match? pattern, file do
+				   name
+				else
+					nil  
+				end
+			end
+
+cat_finder.("hello.ex", {~r/\.ex/i, "Elixir"}) |> IO.puts
 
 x = file_extensions |> 
 Stream.map(fn x -> System.cmd "find", ["../", "-type", "f", "-name", "*.#{x}"]  end) |>
 Stream.map (fn x -> Kernel.elem x, 0 end)
-y = x |> Stream.filter(fn x -> x != "" end) |>
+files = x |> Stream.filter(fn x -> x != "" end) |>
 Enum.map(fn x -> String.split x, "\n"  end)
 
-IO.inspect y
+IO.inspect files
+
 
